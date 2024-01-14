5766b442ab3a4b880605790cd56ad33722afb061
AVan-Git <hoangkc35@gmail.com>
2024-01-14 15:42:23 +0700

    start test 2

---
+++ test2/server_test2/src/test/java/vn/epartner/ServerTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.epartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
